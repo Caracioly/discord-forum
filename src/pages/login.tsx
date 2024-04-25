8292c7da2f7710d574fd5d301aa3e54e30f00f3a
André Caracioly <andrecaracioly@gmail.com>
2024-04-25 12:46:09 -0300

    login screen ✅

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,7 +11,7 @@ export default function Login() {
       />
 
       <div className="absolute bg-[#313338] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-4">
-        <div className="flex flex-col justify-center items-center p-4">
+        <div className="flex flex-col justify-center items-center p-4 select-none">
           <h1 className="text-white text-xl font-bold">
             Boas-vindas de volta!
           </h1>
@@ -19,16 +19,33 @@ export default function Login() {
             Estamos muito animados em te ver novamente!
           </h2>
           <div className="flex flex-col w-[80%] gap-1 mt-4">
-            <h1 className="text-white font-semibold">Email *</h1>
+            <h1 className="text-[#bbbdbe] font-semibold">
+              E-mail
+              <span className="text-[#F23F42] ml-1">*</span>
+            </h1>
             <LoginInput type="email"></LoginInput>
           </div>
           <div className="flex flex-col w-[80%] gap-1 mt-4">
-            <h1 className="text-white font-semibold">Senha *</h1>
+            <h1 className="text-[#bbbdbe] font-semibold">
+              Senha
+              <span className="text-[#F23F42] ml-1">*</span>
+            </h1>
             <LoginInput type="password"></LoginInput>
-            <button className="text-[#0896DE] font-bold text-sm mt-1 self-start">
+            <button className="text-[#0896DE] font-bold text-sm mt-1 self-start hover:underline">
               Esqueceu sua senha?
             </button>
-            <Link  className="text-white font-bold bg-[#4752C4] w-full p-2 rounded-md mt-4 text-center" to="/home">Entrar</Link>
+            <Link
+              className="text-white font-bold bg-[#4752C4] w-full p-2 rounded-md mt-4 text-center hover:bg-[#656dc9]"
+              to="/home"
+            >
+              Entrar
+            </Link>
+            <h1 className="text-sm text-[#8e9192]">
+              Precisando de uma conta?
+              <button className="text-[#0896DE] font-bold text-sm mt-1 self-start hover:underline ml-2">
+                Registre-se
+              </button>
+            </h1>
           </div>
         </div>
       </div>
